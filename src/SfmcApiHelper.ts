02d944a6a88fe0a886368b0aab63ff91d3922d9f
HariRaj01 <hariraj01.pashtek@gmail.com>
2021-09-14 18:05:43 +0530

    Update SfmcApiHelper.ts

--- src/SfmcApiHelper.ts
+++ src/SfmcApiHelper.ts
@@ -94,7 +94,7 @@ export default class SfmcApiHelper
       console.log("Hello");
       console.log("bodymemberid:" + this.member_id);
       console.log("bodymemberid:" + this.soap_instance_url);
-      let oauthToken= this.;
+      let oauthToken= this.oauthAccessToken;
       let self = this;
       // self.getRefreshTokenHelper(this._accessToken, res);
       self

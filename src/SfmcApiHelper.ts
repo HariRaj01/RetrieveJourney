21a8e16a762a39a5e5a52de685947dc96674cc9c
HariRaj01 <hariraj01.pashtek@gmail.com>
2021-09-15 15:34:03 +0530

    parent

--- src/SfmcApiHelper.ts
+++ src/SfmcApiHelper.ts
@@ -564,7 +564,7 @@ export default class SfmcApiHelper
         '    <s:Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">' +
         '        <CreateRequest xmlns="http://exacttarget.com/wsdl/partnerAPI">' +
         '            <Objects xsi:type="DataExtension">' +
-        "                <CategoryID>" + this.FolderID +
+        "                <CategoryID>" + req.body.ParentFolderID +
         "</CategoryID>" +
         "                <CustomerKey>DataExtension_forCheck "+
         "</CustomerKey>" +

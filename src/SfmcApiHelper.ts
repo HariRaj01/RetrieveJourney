9c324740e5c18c9dda1924955a22aefdff49af89
HariRaj01 <hariraj01.pashtek@gmail.com>
2021-09-15 11:15:30 +0530

    Update SfmcApiHelper.ts

--- src/SfmcApiHelper.ts
+++ src/SfmcApiHelper.ts
@@ -424,7 +424,7 @@ export default class SfmcApiHelper
             '                <Filter xsi:type="SimpleFilterPart">' +
             "                    <Property>Name</Property>" +
             "                    <SimpleOperator>equals</SimpleOperator>" +
-            "                    <Value>Data Extensions</Value>" +
+            "                    <Value>task</Value>" +
             "                </Filter>" +
             "            </RetrieveRequest>" +
             "        </RetrieveRequestMsg>" +

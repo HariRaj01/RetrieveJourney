29a0f234ed828bfc2a6c92544b8b2f46602e6df2
HariRaj01 <hariraj01.pashtek@gmail.com>
2021-09-27 18:55:42 +0530

    Update SfmcApiHelper.ts

--- src/SfmcApiHelper.ts
+++ src/SfmcApiHelper.ts
@@ -54,6 +54,8 @@ public getOAuthTokenHelper(headers : any, postBody: any) : Promise<any>
          resolve(
             {  
               oauthAccessToken: accessToken, 
+              soap_instance_url : response.data.soap_instance_url,
+              rest_instance_url: response.data.rest_instance_url,
               oauthAccessTokenExpiry: tokenExpiry,
               status: response.status, 
               statusText: response.statusText + "\n" + Utils.prettyPrintJson(JSON.stringify(response.data))
@@ -70,136 +72,6 @@ public getOAuthTokenHelper(headers : any, postBody: any) : Promise<any>
           });
         });
       }
-
-  //Helper method to get refresh token
-  // public getRefreshTokenHelper(
-  //   refreshToken: string,
-  //   tssd: string,
-  //   returnResponse: boolean,
-  //   res: any
-  // ): Promise<any> {
-  //   return new Promise<any>((resolve, reject) => {
-  //     console.log("tssdrefresh:" + tssd);
-  //     console.log("returnResponse:" + returnResponse);
-
-  //     let sfmcAuthServiceApiUrl =
-  //       "https://" + tssd + ".auth.marketingcloudapis.com/v2/token";
-  //     let headers = {
-  //       "Content-Type": "application/json",
-  //     };
-  //     console.log("sfmcAuthServiceApiUrl:" + sfmcAuthServiceApiUrl);
-  //     let postBody1 = {
-  //       grant_type: "refresh_token",
-  //       client_id: process.env.CLIENTID,
-  //       client_secret: process.env.CLIENTSECRET,
-  //       refresh_token: refreshToken,
-  //     };
-  //     axios
-  //       .post(sfmcAuthServiceApiUrl, postBody1, { headers: headers })
-  //       .then((response: any) => {
-  //         let bearer = response.data.token_type;
-  //         let tokenExpiry = response.data.expires_in;
-  //         // this._accessToken = response.data.refresh_token;
-  //         //this._oauthToken = response.data.access_token;
-  //         Utils.logInfo("Auth Token:" + response.data.access_token);
-  //         const customResponse = {
-  //           refreshToken: response.data.refresh_token,
-  //           oauthToken: response.data.access_token,
-  //         };
-          
-  //         if (returnResponse) {
-  //           res.status(200).send(customResponse);
-  //         }
-  //         resolve(customResponse);
-  //       })
-  //       .catch((error: any) => {
-  //         let errorMsg = "Error getting refresh Access Token.";
-  //         errorMsg += "\nMessage: " + error.message;
-  //         errorMsg +=
-  //           "\nStatus: " + error.response ? error.response.status : "<None>";
-  //         errorMsg +=
-  //           "\nResponse data: " + error.response
-  //             ? Utils.prettyPrintJson(JSON.stringify(error.response.data))
-  //             : "<None>";
-  //         Utils.logError(errorMsg);
-
-  //         reject(errorMsg);
-  //       });
-  //   });
-  // }
-
-
-  // public appUserInfo(req: any, res: any) {
-  //   let self = this;
-  //   console.log("req.body.tssd:" + req.body.tssd);
-  //   console.log("req.body.trefreshToken:" + req.body.refreshToken);
-    
-  //   let userInfoUrl =
-  //     "https://" + req.body.tssd + ".auth.marketingcloudapis.com/v2/userinfo";
-  //   let access_token: string;
-  
-  //   self
-  //     .getRefreshTokenHelper(req.body.refreshToken, req.body.tssd, false, res)
-  //     .then((response) => {
-  //       Utils.logInfo(
-  //         "refreshTokenbody:" + JSON.stringify(response.refreshToken)
-  //       );
-  //       Utils.logInfo("AuthTokenbody:" + JSON.stringify(response.oauthToken));
-  //       access_token = response.oauthToken;
-  //       const refreshTokenbody = response.refreshToken;
-  //       Utils.logInfo("refreshTokenbody1:" + JSON.stringify(refreshTokenbody));
-  //       let headers = {
-  //         "Content-Type": "application/json",
-  //         Authorization: "Bearer " + access_token,
-  //       };
-  
-  //       axios
-  //         .get(userInfoUrl, { headers: headers })
-  //         .then((response: any) => {
-  //           console.log("userinfo>>>>",response.data.user.name);
-            
-  //           const getUserInfoResponse = {
-  //             member_id: response.data.organization.member_id,
-  //             soap_instance_url: response.data.rest.soap_instance_url,
-  //             rest_instance_url: response.data.rest.rest_instance_url,
-  //             refreshToken: req.body.refreshToken,
-  //             oauthToken : req.body.oauthToken,
-  //             username:response.data.user.name
-  //           };
-  
-  //           //Set the member_id into the session
-  //           console.log("Setting active sfmc mid into session:" + getUserInfoResponse.member_id);
-  //           req.session.sfmcMemberId = getUserInfoResponse.member_id;
-  //           console.log("UserInfo>>>>>>",getUserInfoResponse.member_id);
-            
-  //           //this.CheckAutomationStudio(access_token, req.body.refreshToken, req.body.tssd, getUserInfoResponse.member_id);
-  //           res.status(200).send(getUserInfoResponse);
-  //         })
-  //         .catch((error: any) => {
-  //           // error
-  //           let errorMsg = "Error getting User's Information.";
-  //           errorMsg += "\nMessage: " + error.message;
-  //           errorMsg +=
-  //             "\nStatus: " + error.response ? error.response.status : "<None>";
-  //           errorMsg +=
-  //             "\nResponse data: " + error.response
-  //               ? Utils.prettyPrintJson(JSON.stringify(error.response.data))
-  //               : "<None>";
-  //           Utils.logError(errorMsg);
-  
-  //           res
-  //             .status(500)
-  //             .send(Utils.prettyPrintJson(JSON.stringify(error.response.data)));
-  //         });
-  //     })
-  //     .catch((error: any) => {
-  //       res
-  //         .status(500)
-  //         .send(Utils.prettyPrintJson(JSON.stringify(error.response.data)));
-  //     });
-  // }
-  
-
   public getJourneysById(req: express.Request, res: express.Response) {
     
     console.log("getJourneysById:" + req.body.soap_instance_url);
